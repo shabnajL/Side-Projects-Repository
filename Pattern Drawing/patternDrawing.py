2ebe86721d8fc7b0eb2e5731b98a1edf11046a7b
Shabnaj L <shabnajliaqat@gmail.com>
2024-06-17 03:16:50 +0600

    Update patternDrawing.py

--- Pattern Drawing/patternDrawing.py
+++ Pattern Drawing/patternDrawing.py
@@ -3,19 +3,25 @@ from turtle import *
 bgcolor("black")
 speed(0)
 hideturtle()
+colors=['red','purple','green','orange','orchid','yellow']
 
+    
 for i in range(150):
     color("blue")
     circle(i)
-    color("indigo")
+    color(colors[i%6])
+    #color("indigo")
     circle(i*1.0)
-    right(4)
-    forward(4)
-i = 150    
+    width(i/100+1)
+    forward(i)
+    left(59)
+    #right(4)
+    #forward(4)
+i = 89    
 while i>0:
     color("chocolate")
     circle(i)
-    color("orchid")
+    color("blue")
     circle(i*1.0)
     forward(4)
     right(4)
